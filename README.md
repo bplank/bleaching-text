3a0fed9727f1a74f286310a2a897c010c2dc9aa8
R.M. van der Goot <p270396@vesta1.let.rug.nl>
2018-05-01 11:34:52 +0200

    Updated README and runall.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bleaching-text
+
+To reproduce results from the paper (the default number of threads is 4):
+./scripts/runAll.sh <numThreads>
+
+and then:
+./scripts/getResults
+
+
+
--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,35 +1,29 @@
-echo "SHALL WE INCLUDE THE RAW DATA IN THE REPO?"
+if [ "$#" -ne 1 ]; then
+    NTHREADS=4
+else
+    NTHREADS=$1
+fi
+
 python3 scripts/0.featurize.py > run.sh
-chmod +x run.sh
-./run.sh
+parallel -j $NTHREADS < run.sh
 
 python3 scripts/1.ngramTuning.run.py > run.sh
-chmod +x run.sh
-./run.sh
+parallel -j $NTHREADS < run.sh
 
 python3 scripts/2.lexVScomb.run.py > run.sh
-chmod +x run.sh
-./run.sh
+parallel -j $NTHREADS < run.sh
 
 python3 scripts/3.4-1.prep.sh > run.sh
-chmod +x run.sh
-./run.sh
+parallel -j $NTHREADS < run.sh
 
 python3 scripts/3.4-1.run.sh > run.sh
-chmod +x run.sh
-./run.sh
+parallel -j $NTHREADS < run.sh
 
 python3 scripts/4.embeds.prep.py
 python3 scripts/4.embeds.run.py > run.sh
-chmod +x run.sh
-./run.sh
+parallel -j $NTHREADS < run.sh
 
 ./scripts/6.humans.prep.sh
 python3 scripts/6.humans.run.py > run.sh
-chmod +x run.sh
-./run.sh
-
-python3 scripts/9.bigTable.py
-python3 scripts/9.smallTable.py
-python3 scripts/9.humanTable.py
+parallel -j $NTHREADS < run.sh
 
